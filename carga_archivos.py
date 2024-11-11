13e67dadc4676e330217f2c02abefb92b9b71017
coliveraHN <101384458+coliveraHN@users.noreply.github.com>
2024-11-11 11:06:50 -0600

    tercero

--- carga_archivos.py
+++ carga_archivos.py
@@ -148,4 +148,6 @@ def view_files(folder_name):
 
 if __name__ == '__main__':
     #app.run(debug=True)
+    if not os.path.exists(app.config['UPLOAD_FOLDER']):
+        os.makedirs(app.config['UPLOAD_FOLDER'])
     app.run (debug=True, host="0.0.0.0",port=os.getenv("PORT",default=5000))
